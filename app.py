902a8e62e6bebef2308fbbbe04363e7eeded4847
Anshu Kumar <95365748+anshu049@users.noreply.github.com>
2024-07-29 01:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/service1')
 def service1():
-    return "Welcome to Service 1!"
+    return "Welcome to Service 1"
 
 @app.route('/service2')
 def service2():
-    return "Welcome to Service 2!"
+    return "Welcome to Service 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
